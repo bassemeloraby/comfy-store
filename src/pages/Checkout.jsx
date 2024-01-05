ce5080028e7908a7f33cf65492195578625acedf
bassem <basem12683@gmail.com>
2024-01-06 02:08:42 +0300

    Checkout 1

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useSelector } from 'react-redux';
+import { CheckoutForm, SectionTitle, CartTotals } from '../components';
+import { toast } from 'react-toastify';
+import { redirect } from 'react-router-dom';
+
 
 const Checkout = () => {
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text='Your cart is empty' />;
+  }
   return (
-    <div>Checkout</div>
+    <Fragment>
+      <SectionTitle text='place your order' />
+      <div className='mt-8 grid gap-8 md:grid-cols-2 items-start'>
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </Fragment>
   )
 }
 
