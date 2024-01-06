6cb79d1401ac2c065fd271393c23636ebdeaa5e6
bassem <basem12683@gmail.com>
2024-01-06 12:03:03 +0300

    orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { redirect, useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
+import { customFetch } from "../utils";
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+  PaginationContainer,
+} from "../components";
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+      toast.warn("You must logged in to view orders");
+      return redirect("/login");
+    }
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    try {
+     const response = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      console.log(response);
+      return { orders: response.data.data, meta: response.data.meta };
+    } catch (error) {
+      console.log(error);
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "there was an error placing your order";
+      toast.error(errorMessage);
+      if (error?.response?.status === 401 || 403) return redirect("/login");
+      return null;
+    }
+  };
 
 const Orders = () => {
+  const { meta } = useLoaderData();
+  if (meta.pagination.total < 1) {
+    return <SectionTitle text="please make an order" />;
+  }
   return (
-    <div>Orders</div>
-  )
-}
+    <Fragment>
+      {" "}
+      <SectionTitle text="Your Orders" />
+      <OrdersList />
+      <PaginationContainer/>
+      <ComplexPaginationContainer />
+    </Fragment>
+  );
+};
 
-export default Orders
+export default Orders;
